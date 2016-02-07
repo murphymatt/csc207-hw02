ef16e3a9dc943f6d8fad071946400d06f542e682
Zachary Segall <segallza@grinnell.edu>
2016-02-07 17:29:41 -0600

    initial commit; Part 1

---
+++ README.txt
@@ -0,0 +1,5 @@
+Homework: Speed Reader
+Group Members: Matt Murphy, Zachary Segall
+Description: Speed Reader program based on spritz design.
+Resources Used:
+Extra Credit:
---
+++ src/WordGenerator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class WordGenerator {
+	
+	public Scanner in;
+	public int words;
+	public int sentences;
+	
+	public WordGenerator(String filename) throws FileNotFoundException{
+		this.in = new Scanner(new File(filename)); 
+	}
+	
+	public boolean hasNext() throws IOException{
+		return in.hasNext();
+	}
+	
+	public String next() throws IOException{
+		if(!in.hasNext()){return "";}
+		
+		String nextStr = in.next();
+		char lastChar = nextStr.charAt(nextStr.length() - 1);
+				
+		this.words++;
+		if(lastChar == '.' || lastChar == '.' || lastChar == '?'){ this.sentences++; }
+		
+		return nextStr;
+	}
+	
+	public int getWordCount(){
+		return this.words;
+	}
+	
+	public int getSentenceCount(){
+		return this.sentences;
+	}
+	
+}
