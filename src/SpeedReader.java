64ef14ef49f284d928862b4596cd16d199d06d2e
Zachary Segall <segallza@grinnell.edu>
2016-02-07 18:04:06 -0600

    created drawing panel

--- src/SpeedReader.java
+++ src/SpeedReader.java
@@ -1,12 +1,38 @@
+import java.io.FileNotFoundException;
+import java.lang.Thread;
+import java.awt.*;
 
 public class SpeedReader {
 
 	/**
 	 * @param args
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		// Check usage
+		if(args.length != 5) {
+			System.out.println("Usage: SpeedReader <filename> <width> <height> <font size> <wpm>");
+			return;
+		}
+		
+		// Unpack command line arguments
+		String filename = args[0];
+		int width = Integer.parseInt(args[1]);
+		int height = Integer.parseInt(args[2]);
+		int fontSize = Integer.parseInt(args[3]);
+		int wpm = Integer.parseInt(args[4]);
+		
+		// Create drawing panel
+		DrawingPanel panel = new DrawingPanel(width, height);
+		Graphics graphicsContext = panel.getGraphics();
+		Font font = new Font("Courier", Font.BOLD, fontSize);
+		graphicsContext.setFont(font);
+		
+		WordGenerator generator = new WordGenerator(filename);
+		
+		
+		generator.in.close(); 
 	}
 
 }
